0246619d0257ac782a8e17275d689426a631f76c
Allen Xie <allenaxie1@gmail.com>
2022-02-26 11:49:14 -0800

    Create links to navigate to dynamic internal urls

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,6 +1,16 @@
+import { useRouter } from 'next/router';
+
+
 // path:domain/news/newsId
 
 export default function DetailsPage () {
+
+    const router = useRouter();
+
+    const newsId = router.query.newsId;
+
+    // send request to backend API to fetch news item with newsId
+
     return (
         <>
         Details Page
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,7 +1,27 @@
+import Link from 'next/link';
+import { Fragment } from 'react';
+
 export default function NewsPage () {
     return (
-        <>
+        <Fragment>
         News Page
-        </>
+        <ul>
+            <li>
+                <Link href="/news/1">
+                    NextJS is greattttt!
+                </Link>
+            </li>
+            <li>
+                <Link href="/news/2">
+                    NextJS is mehhhhh!
+                </Link>
+            </li>
+            <li>
+                <Link href="/news/3">
+                    NextJS is blahhhhh!
+                </Link>
+            </li>
+        </ul>
+        </Fragment>
     )
 }
