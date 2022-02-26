939352f7b21839ce2fcf240fc53873e97c76a3c6
Allen Xie <allenaxie1@gmail.com>
2022-02-26 12:20:52 -0800

    Copy starting meetup code

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,36 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+    {
+        id:'m1',
+        title: 'A First Meetup',
+        image: 'https://i.imgur.com/OFpzVta.jpg',
+        address: 'Some address',
+        description: 'this is a first meetup',
+
+    },
+    {
+        id:'m2',
+        title: 'A second Meetup',
+        image: 'https://i.imgur.com/OFpzVta.jpg',
+        address: 'Some address',
+        description: 'this is a first meetup',
+
+    },
+    {
+        id:'m3',
+        title: 'A third Meetup',
+        image: 'https://i.imgur.com/OFpzVta.jpg',
+        address: 'Some address',
+        description: 'this is a first meetup',
+
+    },
+]
+
 export default function HomePage () {
     return(
         <>
-            Home Page
+           <MeetupList meetups={DUMMY_MEETUPS}/>
         </>
     )
 }
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,1 @@
+// path: domain/new-meetup
