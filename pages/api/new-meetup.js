3f9308c376b141ed57573ca9e52a3849e39efbd1
Allen Xie <allenaxie1@gmail.com>
2022-02-26 21:26:34 -0800

    Connect to MongoDB database with api

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,20 @@
+import { MongoClient } from 'mongodb';
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-export default function handler(req,res) {
+export default async function handler(req,res) {
     if (req.method === 'POST') {
         const data = req.body;
-        const { title, image, address, description } = data;
+        // Connect to mongoDB - NEVER run on client side - this file will not run on client side so it is secure
+        const client = await MongoClient.connect('mongodb+srv://allen:gasei1@development.7tcu8.mongodb.net/meetups?retryWrites=true&w=majority');
+        const db = client.db();
+        const meetupsCollection = db.collection('meetups');
+        const result = await meetupsCollection.insertOne({data});
+        console.log(result);
 
-        
+        client.close();
+
+        res.status(201).json({message: 'Meetup inserted!'});
     }
 }
